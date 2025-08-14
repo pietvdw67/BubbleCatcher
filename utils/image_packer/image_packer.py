63723bbc2a4636f24c48224dced4cc0de70b0924
pietvdw67 <pietvdw67@gmail.com>
2025-08-14 11:35:53 +0200

    Added image packer UI

--- utils/image_packer/image_packer.py
+++ utils/image_packer/image_packer.py
@@ -3,7 +3,6 @@ import os
 
 pygame.init()
 
-
 class ImagePacker:
 
     SOURCE_FOLDER = 'C:/Users/f3953289/Downloads/FreeTileset/png/Tiles'
@@ -15,13 +14,13 @@ class ImagePacker:
         screen_width, screen_height = 800, 600
         screen = pygame.display.set_mode((screen_width, screen_height))
 
-    def pack(self):
-        entries = os.listdir(ImagePacker.SOURCE_FOLDER)
+    def pack(self, source_folder, target_file, cols, rows):
+        entries = os.listdir(source_folder)
         images_list = []
         max_height = 0
         max_width = 0
         for entry in entries:
-            file_name = os.path.join(ImagePacker.SOURCE_FOLDER, entry)
+            file_name = os.path.join(source_folder, entry)
             image = pygame.image.load(file_name).convert_alpha()
             images_list.append(image)
             if image.get_height() > max_height:
@@ -30,36 +29,18 @@ class ImagePacker:
                 max_width = image.get_width()
 
 
-        image_target = pygame.Surface((max_width * ImagePacker.OUTPUT_COL, max_height * ImagePacker.OUTPUT_ROW), pygame.SRCALPHA)
+        image_target = pygame.Surface((max_width * cols, max_height * rows), pygame.SRCALPHA)
         current_index = 0
-        for row in range(ImagePacker.OUTPUT_ROW):
-            for col in range(ImagePacker.OUTPUT_COL):
+        for row in range(rows):
+            for col in range(cols):
                 if current_index < len(images_list):
                     image_target.blit(images_list[current_index], (col * max_height, row*max_height))
                     current_index += 1
 
         pygame.image.save(image_target, ImagePacker.TARGET_FILE)
-        print(f'Output file created, tile size is Width: {max_width}, Height: {max_height}')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return f'Output file created, tile size is Width: {max_width}, Height: {max_height}'
 
 
 if __name__ == '__main__':
-    ImagePacker().pack()
-
-
-
-
+    result = ImagePacker().pack(ImagePacker.SOURCE_FOLDER, ImagePacker.TARGET_FILE, ImagePacker.OUTPUT_COL,ImagePacker.OUTPUT_ROW)
+    print(result)
---
+++ utils/image_packer/image_packer_ui.py
@@ -0,0 +1,128 @@
+import sys
+from PyQt6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QSpacerItem,
+    QSizePolicy,
+    QFileDialog,
+    QMessageBox
+)
+
+from utils.image_packer.image_packer import ImagePacker
+
+
+class ImagePackerUI(QMainWindow):
+
+    LABEL_WIDTH = 100
+    BUTTON_WIDTH = 150
+
+    def __init__(self):
+        super().__init__()
+
+        # --- Window Setup ---
+        self.setWindowTitle("Image Packer")
+        self.setGeometry(100, 100, 800, 200)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        main_layout = QVBoxLayout(central_widget)
+
+        horizontal_container = QHBoxLayout()
+        label = QLabel("Source Folder")
+        label.setFixedWidth(ImagePacker.LABEL_WIDTH)
+        self.edit_source = QLineEdit()
+        self.btn_get_source = QPushButton("Get Source Folder")
+        self.btn_get_source.setFixedWidth(ImagePacker.BUTTON_WIDTH)
+        horizontal_container.addWidget(label)
+        horizontal_container.addWidget(self.edit_source)
+        horizontal_container.addWidget(self.btn_get_source)
+        main_layout.addLayout(horizontal_container)
+
+        horizontal_container = QHBoxLayout()
+        label = QLabel("Target file")
+        label.setFixedWidth(ImagePacker.LABEL_WIDTH)
+        self.edit_target = QLineEdit()
+        self.btn_get_target = QPushButton("Set target file")
+        self.btn_get_target.setFixedWidth(ImagePacker.BUTTON_WIDTH)
+        horizontal_container.addWidget(label)
+        horizontal_container.addWidget(self.edit_target)
+        horizontal_container.addWidget(self.btn_get_target)
+        main_layout.addLayout(horizontal_container)
+
+        horizontal_container = QHBoxLayout()
+        label = QLabel("Columns")
+        label.setFixedWidth(ImagePacker.LABEL_WIDTH)
+        self.edit_cols = QLineEdit()
+        self.edit_cols.setFixedWidth(ImagePacker.LABEL_WIDTH)
+        spacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        horizontal_container.addWidget(label)
+        horizontal_container.addWidget(self.edit_cols)
+        horizontal_container.addItem(spacer)
+        main_layout.addLayout(horizontal_container)
+
+        horizontal_container = QHBoxLayout()
+        label = QLabel("Rows")
+        label.setFixedWidth(ImagePacker.LABEL_WIDTH)
+        self.edit_rows = QLineEdit()
+        self.edit_rows.setFixedWidth(ImagePacker.LABEL_WIDTH)
+        spacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        horizontal_container.addWidget(label)
+        horizontal_container.addWidget(self.edit_rows)
+        horizontal_container.addItem(spacer)
+        main_layout.addLayout(horizontal_container)
+
+        spacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+        main_layout.addItem(spacer)
+
+        horizontal_container = QHBoxLayout()
+        spacer = QSpacerItem(40, 20, QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Minimum)
+        self.btn_pack = QPushButton("Pack")
+        self.btn_pack.setFixedWidth(ImagePacker.BUTTON_WIDTH)
+        horizontal_container.addItem(spacer)
+        horizontal_container.addWidget(self.btn_pack)
+        main_layout.addLayout(horizontal_container)
+
+        self.btn_get_source.clicked.connect(self.btn_get_source_clicked)
+        self.btn_get_target.clicked.connect(self.btn_set_target_clicked)
+        self.btn_pack.clicked.connect(self.btn_pack_clicked)
+
+    def btn_get_source_clicked(self):
+        file_dialog = QFileDialog()
+        file_dialog.setFileMode(QFileDialog.FileMode.Directory)
+        clicked_button = file_dialog.exec()
+        if clicked_button:
+            selected_files = file_dialog.selectedFiles()
+            self.edit_source.setText(selected_files[0])
+
+    def btn_set_target_clicked(self):
+        file_dialog = QFileDialog()
+        clicked_button = file_dialog.exec()
+        if clicked_button:
+            selected_files = file_dialog.selectedFiles()
+            self.edit_target.setText(selected_files[0])
+
+    def btn_pack_clicked(self):
+
+        image_packer = ImagePacker()
+        result = image_packer.pack(self.edit_source.text(), self.edit_target.text(), self.edit_cols.text(), self.edit_rows.text())
+
+        dialog = QMessageBox()
+        dialog.setText("Packing completed")
+        dialog.setWindowTitle("Please note")
+        dialog.setIcon(QMessageBox.Icon.Information)
+
+        dialog.setStandardButtons(QMessageBox.StandardButton.Ok)
+        clicked_button = dialog.exec()
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = ImagePackerUI()
+    window.show()
+    sys.exit(app.exec())
